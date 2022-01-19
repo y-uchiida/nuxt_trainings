ed7e2d559e9685ae3b72ec27ad368798530bbc46
yugo uchiida <y.uchiida@nirneege.com>
2022-01-19 17:22:19 +0900

    dynamic url parameters sample

---
+++ trainig001/pages/users/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>
+      <!-- /users/_id.vue -->
+      User ID: {{ userId }}
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data(){
+    return {
+      userId: this.$route.params.id
+    }
+  }
+}
+</script>
