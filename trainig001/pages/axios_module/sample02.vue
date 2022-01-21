61a254f16d496b1881cafc4852b6bbc69fa53125
yugo uchiida <y.uchiida@nirneege.com>
2022-01-21 23:36:26 +0900

    add axios sample2

---
+++ trainig001/pages/axios_module/sample02.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>Nuxt.js のタグが付けられた投稿の一覧</h3>
+      <p>
+        axios モジュールを利用してQiita API からデータを取得しています
+      </p>
+    </div>
+  </section>
+</template>
